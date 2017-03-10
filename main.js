cf3e179dd6b55ea6c8b3da4db59256f35879a448
Travers McInerney <tmcinerney@zendesk.com>
2017-03-10 15:02:32 -0800

    Add context menu, and option to quit app

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const Menubar = require('menubar')
+const { Menu } = require('electron')
+const Menubar  = require('menubar')
+
+const contextMenu = Menu.buildFromTemplate([{
+  label: 'Quit Keep',
+  accelerator: 'Cmd+Q',
+  click: () => {
+    menubar.app.quit()
+  }
+}])
 
 const menubar = Menubar({
   alwaysOnTop: true,
@@ -12,4 +21,10 @@ const menubar = Menubar({
 
 menubar.on('focus-lost', () => {
   menubar.hideWindow()
+})
+
+menubar.on('ready', () => {
+  menubar.tray.on('right-click', () => {
+    menubar.tray.popUpContextMenu(contextMenu)
+  })
 })
